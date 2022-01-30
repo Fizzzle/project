bfe04d6b00149f71e86b32fa6230c8fa8af1be48
Fizzzle <Rickeygame23@gmail.com>
2022-01-30 17:22:35 +0200

    l

---
+++ digits.js
@@ -0,0 +1,21 @@
+let digit = 25;
+let fract = 25.03;
+let exp = 2e10; // 20000000000
+let expSmall = 2e-3; // 0.002
+let expFract = 1.25e3; // 1250
+let expfractSmall = 1.25e-3; // 0.00125
+let fract = -25.
+let fract2 = -.25;
+
+let dec = 9 // десятичная обычная система
+let bin = 0b1001;
+
+// 8
+
+let oct= 0o7; // 7
+let oct 0o10; // 8
+
+// 16
+
+let x = 0x1b; // 27
+let X = 0X1B; // 27 
